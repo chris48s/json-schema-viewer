5a229b6cc7059430cdaab91d6c52be54477b3c30
chris48s <chris.shaw480@gmail.com>
2022-01-23 20:19:32 +0000

    add test for renderer

---
+++ tests/test_renderer.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from lib import renderer
+
+
+def test_renderer():
+    # very basic smoke test
+    # this will basically just flag breaking API changes in json_schema_for_humans
+
+    html = renderer.render_schema(Path.cwd() / "tests" / "testfiles" / "example.json")
+    assert "<!DOCTYPE html>" in html
